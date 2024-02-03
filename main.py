77b32ee3b65a4e570c8e8963cae17c404665330d
Ray7808 <yuwang7808@gmail.com>
2024-02-03 14:34:16 +0800

    feat: add gpu acceleration for CUDA and M1 GPU

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import torch
 
+print('-'*30)
+print('Now start the program')
+print('-'*30)
+def get_device():
+    """
+        Check if CUDA or mac m1 GPU is available
+    """
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        return torch.device("mps")
+    else:
+        return torch.device("cpu")
+
+device = get_device()
+print(f"Using device: {device}")
+print('-'*30)
+
 def main():
     print('main')
 
